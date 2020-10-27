f03a938e4734e632914333976ed9d37e1a8a5e71
Igor Zalatov <zig@dolphin.flymon.net>
2020-10-27 23:54:43 +0300

    Add modbus-cli tool

--- tools/make.sh
+++ tools/make.sh
@@ -3,6 +3,6 @@
 # apt-get install libmodbus5 libmodbus-dev
 
 gcc -o modbus-flash modbus-flash.c -Wall -Werror
-gcc -o modbus-cli modbus-cli-v2.c $(pkg-config --cflags --libs libmodbus)
+gcc -o modbus-cli modbus-cli.c $(pkg-config --cflags --libs libmodbus)
 
 strip modbus-flash modbus-cli
